92bda59b23df99479079739ec5059adca0c5e514
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-11-25 13:42:11 +0530

    url fixed

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
 const Register = () => {
     const [username, setUsername] = useState('');
